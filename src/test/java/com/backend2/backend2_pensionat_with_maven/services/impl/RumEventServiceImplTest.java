2a85e7b5e91b11f57f8988df419d57d46538c35c
Aron Gyulai <arongyf@gmail.com>
2024-05-20 18:11:11 +0200

    Test file for RumEventService created

---
+++ src/test/java/com/backend2/backend2_pensionat_with_maven/services/impl/RumEventServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.backend2.backend2_pensionat_with_maven.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RumEventServiceImplTest {
+
+    @Test
+    void findEventsByRoomNr() {
+    }
+
+    @Test
+    void getEventListByRoomNr() {
+    }
+
+    @Test
+    void sparaRumEvent() {
+    }
+
+    @Test
+    void eventToString() {
+    }
+}
