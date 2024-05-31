2edae887f3c7d04b8373ee63cf33ac264729e106
Pejman <pejman.elias@yh.nackademin.se>
2024-05-31 23:23:46 +0200

    mail settings update

--- src/main/java/com/backend2/backend2_pensionat_with_maven/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/backend2/backend2_pensionat_with_maven/event/listener/RegistrationCompleteEventListener.java
@@ -36,7 +36,7 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
     }
 
     public void sendPasswordResetVerificationEmail(String url) throws MessagingException, UnsupportedEncodingException, jakarta.mail.MessagingException {
-        theUser ="o.ekstrom@hotmail.se";
+        theUser ="unique4@ethereal.email";
         String subject = "Återställning av lösenord";
         String senderName = "Pensionatet";
         String mailContent = "<p> Hej.</p>"+
@@ -49,7 +49,7 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
         var messageHelper = new MimeMessageHelper(message);
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println(theUser);
-        messageHelper.setFrom("o.ekstrom@hotmail.se", senderName);
+        messageHelper.setFrom("unique4@ethereal.email", senderName);
         messageHelper.setTo(theUser/*.getUsername()*/);
         messageHelper.setSubject(subject);
         messageHelper.setText(mailContent, true);
