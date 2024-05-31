69da19833eb097fe4a5c1ac016950f9cad6adde2
Aron Gyulai <arongyf@gmail.com>
2024-05-31 23:59:34 +0200

    Works

--- src/main/java/com/backend2/backend2_pensionat_with_maven/repos/PasswordResetTokenRepo.java
+++ src/main/java/com/backend2/backend2_pensionat_with_maven/repos/PasswordResetTokenRepo.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PasswordResetTokenRepo extends JpaRepository<PasswordResetToken, Long> {
     PasswordResetToken findByToken(String theToken);
+    
 }
